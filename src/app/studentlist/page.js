15c13d762e770a3e0e025b361bb1d99c1bc66aef
Chandan kumar <chandankumar700451@gmail.com>
2025-05-21 11:14:26 +0530

    Update page.js

--- src/app/studentlist/page.js
+++ src/app/studentlist/page.js
@@ -20,7 +20,11 @@ export default function StudentList() {
             <Link href="/login/studentlogin">Student 5</Link>
         </li>
       </ul>
-
+       <Link href="/study">Go to go to study page</Link>
+    <br />
+    
+     <Link href="/studentlist">Go to Student List</Link>
+    <br />
       <Link href="/">Go to Home</Link>
     </main>
   );
